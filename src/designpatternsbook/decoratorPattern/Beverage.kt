fff97ca92f569bb000d19840af8555fd1c681f91
Begum Yazici <yazicibegum@gmail.com>
2020-12-22 11:41:19 +0000

    Refactoring package name

--- src/designpatternsbook/decoratorPattern/Beverage.kt
+++ src/designpatternsbook/decoratorPattern/Beverage.kt
@@ -1,20 +1,13 @@
-package designpatternsbook.theDecoratorPattern3
+package designpatternsbook.decoratorPattern
 
 abstract class Beverage {
 
     lateinit var description : String
 
     abstract fun cost() : Double
-
-
-
-
-
 }
 
-
 class Decaf : Beverage() {
-
     override fun cost(): Double {
         return 4.0
     }
